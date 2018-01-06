Fix scaling

torchlight/structured/pandas/encoder.py
@@ -78,9 +78,11 @@ def _fix_na(df, na_dict):
 
 def scale_vars(df, mapper=None):
     # TODO Try RankGauss: https://www.kaggle.com/c/porto-seguro-safe-driver-prediction/discussion/44629
+    num_cols = [n for n in df.columns if is_numeric_dtype(df[n])]
+    df[num_cols] = df[num_cols].astype(np.float32)
     if mapper is None:
         # is_numeric_dtype will exclude categorical columns
-        map_f = [([n], StandardScaler()) for n in df.columns if is_numeric_dtype(df[n])]
+        map_f = [([n], StandardScaler()) for n in num_cols]
         mapper = DataFrameMapper(map_f).fit(df)
     df[mapper.transformed_names_] = mapper.transform(df)
     return mapper
@@ -173,9 +175,6 @@ def apply_encoding(df, cont_features, categ_features, scale_continuous=False,
 
     # Scale continuous vars
     if scale_continuous:
-        for k in cont_features:
-            if k in df.columns:
-                df[k] = df[k].astype(np.float32)
         encoder_blueprint.scale_mapper = scale_vars(df, encoder_blueprint.scale_mapper)
         print(f"List of scaled columns: {encoder_blueprint.scale_mapper.transformed_names_}")
 