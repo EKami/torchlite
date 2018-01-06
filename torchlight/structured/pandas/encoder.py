Fix scaling

torchlight/structured/pandas/encoder.py
@@ -76,12 +76,11 @@ def _fix_na(df, na_dict):
     return df, na_dict
 
 
-def scale_vars(df, mapper=None):
+def scale_vars(df, cont_features, mapper=None):
     # TODO Try RankGauss: https://www.kaggle.com/c/porto-seguro-safe-driver-prediction/discussion/44629
-    warnings.filterwarnings('ignore', category=sklearn.exceptions.DataConversionWarning)
     if mapper is None:
         # is_numeric_dtype will exclude categorical columns
-        map_f = [([n], StandardScaler()) for n in df.columns if is_numeric_dtype(df[n])]
+        map_f = [([n], StandardScaler()) for n in cont_features]
         mapper = DataFrameMapper(map_f).fit(df)
     df[mapper.transformed_names_] = mapper.transform(df)
     return mapper
@@ -150,7 +149,6 @@ def apply_encoding(df, cont_features, categ_features, scale_continuous=False,
 
     df, encoder_blueprint.na_dict = _fix_na(df, encoder_blueprint.na_dict)
     print(f"Warning: Missing columns: {missing_col}, dropping them...")
-
     print(f"Categorizing features {categ_features}")
     # If the categorical mapping exists
     if encoder_blueprint.categ_var_map:
@@ -175,7 +173,10 @@ def apply_encoding(df, cont_features, categ_features, scale_continuous=False,
 
     # Scale continuous vars
     if scale_continuous:
-        encoder_blueprint.scale_mapper = scale_vars(df, encoder_blueprint.scale_mapper)
+        for k in cont_features:
+            if k in df.columns:
+                df[k] = df[k].astype(np.float32)
+        encoder_blueprint.scale_mapper = scale_vars(df, cont_features, encoder_blueprint.scale_mapper)
         print(f"List of scaled columns: {encoder_blueprint.scale_mapper.transformed_names_}")
 
     # Save categorical codes into encoderBlueprint