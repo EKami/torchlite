cfb9d25c10d45ba683686a2cb6b11234e4190766
Tuatini Godard <tuatinigodard@gmail.com>
2018-02-08 09:42:01 +0100

    Add super resolution file

---
+++ examples/super_resolution.py
@@ -0,0 +1,18 @@
+"""
+This script implements a neural network to enhance images:
+ - https://arxiv.org/abs/1712.05927
+
+Other interesting papers:
+ - https://arxiv.org/abs/1801.10319
+ - https://github.com/DmitryUlyanov/deep-image-prior
+
+ Seems to be the best one in term of accuracy/speed:
+  - https://github.com/phoenix104104/LapSRN (no hallucination)
+  -> (letsenhance) https://github.com/LimBee/NTIRE2017
+
+  https://github.com/aitorzip/PyTorch-SRGAN
+
+  To implement:
+   - https://github.com/leftthomas/SRGAN (PyTorch)
+   - https://github.com/tensorlayer/srgan (Tensorflow)
+"""
