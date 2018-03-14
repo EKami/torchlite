d3fff2ebced259e86325690fe6c9c596e48a8aa9
Tuatini Godard <tuatinigodard@gmail.com>
2018-03-14 16:18:17 +0100

    Change todo list

--- examples/srpgan.py
+++ examples/srpgan.py
@@ -27,7 +27,7 @@ saved_model_dir = tfiles.create_dir_if_not_exists(os.path.join(cur_path, "checkp
 
 
 def get_loaders(args, num_workers):
-    # TODO take a look and use this datasets: https://superresolution.tf.fau.de/
+    # TODO this dataset consider something else than bicubic interpolation: https://superresolution.tf.fau.de/
     ds_path = Path("/tmp")
     fetcher.WebFetcher.download_dataset("https://s3-eu-west-1.amazonaws.com/torchlite-datasets/DIV2K.zip",
                                         ds_path.absolute(), True)
