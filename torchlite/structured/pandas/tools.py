0f6df998df7c94fc4aa70837633bded6f67150b5
Tuatini Godard <tuatinigodard@gmail.com>
2018-03-29 16:27:59 +0200

    Add get_duplicates

--- torchlite/structured/pandas/tools.py
+++ torchlite/structured/pandas/tools.py
@@ -1,4 +1,6 @@
 import numpy as np
+from tqdm import tqdm
+import pandas as pd
 
 
 def count_missing(df_list):
@@ -15,3 +17,31 @@ def count_missing(df_list):
     total_cells = sum([np.product(df.shape) for df in df_list])
 
     return total_missing / total_cells
+
+
+def get_duplicated_columns(df, drop_duplicates=False):
+    """
+    Get duplicated columns as a dictionary mapping between the duplicates.
+    This implementation has tqdm to monitor the progress and is better fitted
+    for large DataFrames.
+    Args:
+        df (Dataframe): The DataFrame to explore
+        drop_duplicates (bool): Will drop the duplicates if set to True
+
+    Returns:
+        dict: A dictionary containing a mapping between the duplicated columns
+    """
+    enc_df = pd.DataFrame(index=df.index)
+    for col in tqdm(df.columns, desc="Factorizing columns"):
+        enc_df[col] = df[col].factorize()[0]
+
+    dup_cols = {}
+    for i, c1 in enumerate(tqdm(enc_df.columns, desc="Searching for duplicate columns")):
+        for c2 in enc_df.columns[i + 1:]:
+            if c2 not in dup_cols and np.all(enc_df[c1] == enc_df[c2]):
+                dup_cols[c2] = c1
+
+    if drop_duplicates:
+        df.drop(dup_cols.keys(), axis=1, inplace=True)
+
+    return dup_cols
