d2fd456d73f59d33b9476d1a872eb429a7c6ffc9
Tuatini Godard <tuatinigodard@gmail.com>
2018-05-12 15:47:41 +0200

    Add moving average plot

--- torchlite/pandas/date.py
+++ torchlite/pandas/date.py
@@ -1,4 +1,6 @@
 # TODO include https://github.com/blue-yonder/tsfresh
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
 import numpy as np
 import re
@@ -83,3 +85,50 @@ def get_elapsed(df, date_field, from_date=np.datetime64('1970-01-01'), prefix='E
 
     df[prefix + ts_type] = res
     return df
+
+
+def plotMovingAverage(series, window, plot_intervals=False, conf_interval=1.96, plot_anomalies=False):
+    """
+    Plot moving average over a given pandas Series
+    Args:
+        series (pd.Series): A pandas Series with date index
+        window (int): Rolling window size
+        plot_intervals (bool): Show confidence intervals
+        conf_interval (float): The confidence interval:
+            0.95 = 95% interval with z = 1.96
+            0.99 = 99% interval with z = 2.576
+            0.995 = 99.5% interval with z = 2.807
+            0.999 = 99.9% interval with z = 3.291
+        plot_anomalies (bool): show anomalies
+
+    Returns:
+
+    """
+    z = {0.95: 1.96, 0.99: 2.576, 0.995: 2.807, 0.999: 3.291}
+    rolling_mean = series.rolling(window=window).mean()
+
+    plt.figure(figsize=(15, 5))
+    plt.title("Moving average\n window size = {}".format(window))
+    plt.plot(rolling_mean, "g", label="Rolling mean trend")
+
+    # Plot confidence intervals for smoothed values
+    if plot_intervals:
+        mae = mean_absolute_error(series[window:], rolling_mean[window:])
+        deviation = np.std(series[window:] - rolling_mean[window:])
+        print(f"MAE: {mae}, Deviation: {deviation}")
+        # z = 1.96 is the 95% confidence interval
+        lower_bond = rolling_mean - (mae + z[conf_interval] * deviation)
+        upper_bond = rolling_mean + (mae + z[conf_interval] * deviation)
+        plt.plot(upper_bond, "r--", label="Upper Bond / Lower Bond")
+        plt.plot(lower_bond, "r--")
+
+        # Having the intervals, find abnormal values
+        if plot_anomalies:
+            anomalies = pd.DataFrame(index=series.index, columns=series.columns)
+            anomalies[series < lower_bond] = series[series < lower_bond]
+            anomalies[series > upper_bond] = series[series > upper_bond]
+            plt.plot(anomalies, "ro", markersize=10)
+
+    plt.plot(series[window:], label="Actual values")
+    plt.legend(loc="upper left")
+    plt.grid(True)
